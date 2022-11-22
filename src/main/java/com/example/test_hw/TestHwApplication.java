74673bd3f3d0b6b0e433f008ff8831924206184a
Dima <akmeev2011@yandex.ru>
2022-11-22 21:06:08 +0300

    Initial commit

---
+++ src/main/java/com/example/test_hw/TestHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_hw/TestHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
